9502a3809baf87194d24bbc5e2eb5fe616c8c1a0
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-03-11 09:43:05 -0300

    It creates a queryString function and its test

---
+++ src/lib/queryString.spec.js
@@ -0,0 +1,11 @@
+const { queryString } = require('./queryString');
+
+describe('Object to query string', () => {
+    it('should create a valid query string', () => {
+        const obj = {
+            name: "Roger",
+            profession: "developer"
+        };
+        expect(queryString(obj)).toBe('name=Roger&profession=developer');
+    });
+});
